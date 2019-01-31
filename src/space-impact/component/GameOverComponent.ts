a823731cb01a28eeb27f7645a69b0bac8fbf7f9c
Josef Dolezal <pepik.ml@gmail.com>
2019-01-31 16:59:12 +0100

    Fix game over message

--- src/space-impact/component/GameOverComponent.ts
+++ src/space-impact/component/GameOverComponent.ts
@@ -4,7 +4,7 @@ export class GamerOverComponent extends SpaceImpactBaseComponent {
     onUpdate(delta, absolute) {
         super.onUpdate(delta, absolute);
         
-        let description = this.model.isGameOver ? "Game Over!" : "You Won!";
+        let description = this.model.lives <= 0 ? "Game Over!" : "You Won!";
 
         let text = <PIXI.Text>this.owner.getPixiObj();
         text.text = description + "\nYour score: " + this.model.score;
